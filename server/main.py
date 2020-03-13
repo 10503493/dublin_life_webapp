ceb926c13fb5c34d197ccd37d16fe520662808b5
Tony <56820131+10503493@users.noreply.github.com>
2020-03-13 15:58:44 +0000

    server

--- server/main.py
+++ server/main.py
@@ -7,37 +7,30 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_DB'] = 'web_ise'
-mysql = MySQL(app)
+mysql = MySQL(app)#for the route test
 @app.route('/api/', methods=['GET', 'POST'])
 def index():
     cur = mysql.connection.cursor()
     cur.execute("select * from users ")
     d = cur.fetchall()
-    print (d)#jkkkkkkkkkkkkkkkkkkkjkjkkkkkkk
+    print (d)
     cur.close()
     return jsonify (d)
 @app.route('/api/userdet', methods=['GET', 'POST'])
 def userdef():
-    return  ('testttt')
-
-
-
-
-
-
+    return 'ok'
 @app.route('/api/user', methods=['GET', 'POST'])
 def usercall():
     cur = mysql.connection.cursor()
     cur.execute("select * from users where uname='tonydbs'")
     d = cur.fetchall()
     u=d[0][0]
-    
     print('usercall')
-    print (d[0][2])#jkkkkkkkkkkkkkkkkkkkjkjkkkkkkk
+    print (d[0][2])
     cur.close()
     return  jsonify (d)
     
-################################################
+#for trigger call
 @app.route('/api/reg', methods=['GET', 'POST'])
 def reg():
     usr = request.form.get('uname')
@@ -50,19 +43,16 @@ def reg():
     mysql.connection.commit()
     cur.close()
     print('inserted')
-################################################
+#admin request
 @app.route('/api/admin', methods=['GET', 'POST'])
 def adm():
     cur = mysql.connection.cursor()
     cur.execute("select * from admin ")
     d = cur.fetchall()
-    print (d)#jkkkkkkkkkkkkkkkkkkkjkjkkkkkkk
+    print (d)
     cur.close()
     return jsonify (d)
-################################################
-
-
-
+#register
 @app.route('/api/register', methods=['POST'])
 def register():
     fn = request.form.get('fname_r')
@@ -88,20 +78,20 @@ def register():
         cur.close()    
         print('donr---reg already in table not inserted')
         return 'jsonify(data)'
-#####################################################################
+#login
 @app.route('/api/login', methods=['GET','POST'])
 def login():
-    print("now reached in flask")#kkkkkkkkkkkkkkkkkkk
+    print("now reached in flask")
     usr = request.form.get('uname')
     psd = request.form.get('psw')
-    print (usr, psd)#jokkjjjjjjjjmmmjjjjjjjjjjjjjjjjj
+    print (usr, psd)
     cur = mysql.connection.cursor()
     cur.execute("select * from users where uname=%s and pword=%s",[usr.strip(),psd.strip()])
     data = cur.fetchall() 
-    print (data)#jkkkkkkkkkkkkkkkkkkkjkjkkkkkkk
+    print (data)
     cur.close()
     print('sql close')
-    print (len(data))##khhhhhhhhhhkkkkhkhk
+    print (len(data))#
     if len(data) > 0:
         #print('if true')
         return( jsonify(data))
@@ -110,47 +100,5 @@ def login():
         print ('if false')
         return (jsonify(data))
         
-
-    
-# @app.route('/products', methods=['GET', 'POST'])
-# def products():
-#     return  render_template('products.html')
-
-# @app.route('/sign_up', methods=['GET', 'POST'])
-# def sign_up():
-#      return  render_template('sign_up.html')
-# @app.route('/render-user-creation', methods=['GET', 'POST'])
-# def renderusercreation():
-#     return  render_template('user-creation.html')
-
-        
-    
-@app.route('/signup', methods=['POST'])
-def signup():
-    return render_template('sign_up.html')
-@app.route('/usercreation',methods=['POST'])
-def usercreation():    
-    print ("start")
-    usr = request.form.get('uname')
-    psw = request.form.get('psw')
-    email=request.form.get('Email')
-    print("c=begore")
-    cur = mysql.connection.cursor()
-    print("after")
-    cur.execute("select * from users where uname=%s or email=%s",[usr,email])
-    data = cur.fetchall()
-    print(data)
-    print ("here")
-
-    if len(data) == 0:
-        print("anddddd")
-        cur.execute("insert into users (uname,pword,firstname,lastname,email,address) values (%s,%s,%s,%s,%s,%s)",(usr,psw,email,usr,email,psw))
-        mysql.connection.commit()
-        cur.close()
-        print("now")
-        return render_template('loginpage.html')
-    else:
-        cur.close()
-        return render_template('test1.html') 
 if __name__ == '__main__':
     app.run(debug=True)
